d27e8fbfd8c639ce2b38ec83494ede28741b80f7
TinoMartino094 <78217336+TinoMartino094@users.noreply.github.com>
2025-06-23 16:41:51 -0500

    Update VaultFiltersRuneAddon.java

--- src/main/java/net/tino/runeaddon/VaultFiltersRuneAddon.java
+++ src/main/java/net/tino/runeaddon/VaultFiltersRuneAddon.java
@@ -1,15 +1,29 @@
 package net.tino.runeaddon;
 
-import net.tino.runeaddon.attributes.rune.BossRuneBoosterPackTypeAttribute;
-import net.tino.runeaddon.attributes.rune.BossRuneGearRarityAttribute;
-import net.tino.runeaddon.attributes.rune.BossRuneGivesItemAttribute;
-import net.tino.runeaddon.attributes.rune.BossRuneInscriptionTypeAttribute;
-import net.tino.runeaddon.attributes.rune.BossRuneModifierAttribute;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.tino.runeaddon.attributes.rune.*;
+import net.tino.runeaddon.ModPresence;
 
-@Mod("vaultfiltersruneaddon")
+@Mod(VaultFiltersRuneAddon.MOD_ID)
 public class VaultFiltersRuneAddon {
+    public static final String MOD_ID = "vaultfiltersruneaddon";
+    public static final String MOD_VERSION = "1.21.1-addon-1";
+
     public VaultFiltersRuneAddon() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(ModPresence.class);
+    }
+
+    private void setup(FMLCommonSetupEvent event) {
+        ModPresence.init();
         // Register your custom attributes with dummy/default values!
         new BossRuneModifierAttribute("Strength").register(BossRuneModifierAttribute::new);
         new BossRuneGivesItemAttribute("the_vault:helmet").register(BossRuneGivesItemAttribute::new);
