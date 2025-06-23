315128432500302b0ccee814bd84414f3b8c1854
TinoMartino094 <78217336+TinoMartino094@users.noreply.github.com>
2025-06-23 16:58:10 -0500

    Update MixinFilterItem.java

--- src/main/java/net/tino/runeaddon/mixin/compat/create/MixinFilterItem.java
+++ src/main/java/net/tino/runeaddon/mixin/compat/create/MixinFilterItem.java
@@ -1,7 +1,18 @@
 package net.tino.runeaddon.mixin.compat.create;
 
 import com.simibubi.create.content.logistics.filter.FilterItem;
+import com.simibubi.create.content.logistics.filter.ItemAttribute;
 import net.tino.runeaddon.ModPresence;
+import net.tino.runeaddon.VaultFiltersRuneAddon;
+import net.tino.runeaddon.attributes.rune.*;
+import net.joseph.vaultfilters.attributes.abstracts.StringAttribute;
+import net.minecraft.ChatFormatting;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -16,9 +27,34 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinFilterItem {
     @Inject(method = "use", at = @At("HEAD"), cancellable = true)
     public void runeAddon$use(Level world, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
-        if (!ModPresence.PLAYERS_WITH_RUNE_ADDON.contains(player.getUUID())) {
-            // Optionally show a message here
-            cir.setReturnValue(InteractionResultHolder.pass(player.getItemInHand(hand)));
+        if (ModPresence.PLAYERS_WITH_RUNE_ADDON.contains(player.getUUID())) {
+            return;
+        }
+
+        ItemStack heldItem = player.getItemInHand(hand);
+        if (player.isShiftKeyDown()
+                || hand != InteractionHand.MAIN_HAND
+                || world.isClientSide
+                || !(player instanceof ServerPlayer)
+                || !heldItem.hasTag()) {
+            return;
+        }
+
+        Component msg = new TextComponent(
+            "This filter has Vault Filters Rune Addon features selected on it. " +
+            "Install Vault Filters Rune Addon version " + VaultFiltersRuneAddon.MOD_VERSION +
+            " to open the UI (Sneaky mad Tino >:c)"
+        ).withStyle(ChatFormatting.RED);
+
+        CompoundTag tag = heldItem.getTag();
+        // Check if the filter has your rune attributes (stored as StringAttribute)
+        ListTag attributes = tag.getList("MatchedAttributes", CompoundTag.TAG_COMPOUND);
+        for (Tag attribute : attributes) {
+            if (attribute instanceof CompoundTag compound && ItemAttribute.fromNBT(compound) instanceof StringAttribute) {
+                player.displayClientMessage(msg, false);
+                cir.setReturnValue(InteractionResultHolder.pass(heldItem));
+                break;
+            }
         }
     }
 }
